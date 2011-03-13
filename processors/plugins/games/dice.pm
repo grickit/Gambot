9a3cc11a1315d1288e75c731c8244587a7b9f8a6
Grickit <thegrickit@gmail.com>
2011-03-13 12:56:32 -0400

    games plugin

---
+++ processors/plugins/games/dice.pm
@@ -0,0 +1,17 @@
+push (@commands_regexes, "$sl !d([0-9]+)");
+push (@commands_subs, sub {
+  $answer = int(rand($1))+1;
+  ACT("MESSAGE","$target","$receiver: $answer"); 
+});
+
+push (@commands_regexes, "$sl !([0-9]+)d([0-9]+)");
+push (@commands_subs, sub {
+  my ($i, $rand);
+  while($i < $1) {
+  $rand = int(rand($1))+1;
+  $answer += $rand;
+  $i += 1;
+  #ACT("MESSAGE","$target","$receiver: roll $i yielded $rand\n"); 
+  }
+  ACT("MESSAGE","$target","$receiver: The total is $answer"); 
+});
