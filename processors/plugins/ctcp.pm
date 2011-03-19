16af2b7b6a5e60043de2d65466106d7fee1a8b81
Grickit <thegrickit@gmail.com>
2011-03-18 21:23:52 -0400

    Fixes issue #12 with responding to public CTCP. Thanks shadowmaster for the report.

--- processors/plugins/ctcp.pm
+++ processors/plugins/ctcp.pm
@@ -1,21 +1,21 @@
 push (@commands_regexes, "(VERSION|CLIENTINFO) ?");
 push (@commands_subs, sub {
-  ACT("NOTICE",$target,"$1 $version"); 
+  ACT("NOTICE",$sender,"$1 $version"); 
 });
 
 push (@commands_regexes, "TIME ?");
 push (@commands_subs, sub {
   my $timestamp = strftime('%m-%d-%Y %H:%M:%S',localtime); 
-  ACT("NOTICE","$target","TIME $timestamp");
+  ACT("NOTICE",$sender,"TIME $timestamp");
 });
 
 push (@commands_regexes, "PING [0-9]+");
 push (@commands_subs, sub {
   my $timestamp = time; 
-  ACT("NOTICE","$target","PING $timestamp"); 
+  ACT("NOTICE",$sender,"PING $timestamp"); 
 });
 
 push (@commands_regexes, "FINGER ?");
 push (@commands_subs, sub {
-  ACT("NOTICE","$target","FINGER Take your fingers off me!"); 
+  ACT("NOTICE",$sender,"FINGER Take your fingers off me!"); 
 });
