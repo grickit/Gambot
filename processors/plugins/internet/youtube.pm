343b29c9a8bbe71e6d015ba9350e3ffcd9a64559
Grickit <thegrickit@gmail.com>
2011-04-11 13:07:42 -0400

    added !youtube command

---
+++ processors/plugins/internet/youtube.pm
@@ -0,0 +1,35 @@
+push (@commands_regexes, "$sl !youtube ([a-zA-Z0-9]+)");
+push (@commands_subs, sub {
+  my $vid = $1;
+  my $url = "http://gdata.youtube.com/feeds/api/videos/$vid?v=2";
+  my $request = LWP::UserAgent->new;
+  $request->timeout(60);
+  $request->env_proxy;
+  $request->agent('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0)');
+  $request->max_size('1024000');
+  my $response = $request->get("$url");
+  my $content = $response->decoded_content;
+
+  if ($content =~ /<error><domain>GData<\/domain><code>InvalidRequestUriException<\/code><internalReason>Invalid id<\/internalReason><\/error>/) {
+    ACT("MESSAGE",$target,"$receiver: That video does not exist.");
+  }
+
+  elsif ($content =~ /<title>(.+)<\/title>/) {
+    my $title = $1;
+    $content =~ /<name>(.+)<\/name>/;
+    ACT("MESSAGE",$target,"$receiver: The title is \"$title\". It was uploaded by \"$1\".");
+
+    $content =~ /<yt:statistics favoriteCount='([0-9]+)' viewCount='([0-9]+)'\/>/;
+    my ($favorites, $views) = ($2, $1);
+    $content =~ /<yt:rating numDislikes='([0-9]+)' numLikes='([0-9]+)'\/>/;
+    my ($likes, $dislikes) = ($2, $1);
+    ACT("MESSAGE",$target,"$receiver: It has $views views, $likes likes, and $dislikes dislikes.");
+
+    $content =~ /<yt:duration seconds='([0-9]+)'\/>/;
+    my $length = $1;
+    my $length_m = int($length / 60);
+    my $length_s = $length % 60;
+
+    ACT("MESSAGE",$target,"$receiver: It is $length_m minutes and $length_s seconds long.");
+  }
+});
