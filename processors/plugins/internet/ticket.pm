caf5a4307cfec0405358be277fc1c06e71baffa9
Grickit <thegrickit@gmail.com>
2011-04-10 21:21:12 -0400

    fixed a bug with sending two urls instead of looking up the title

--- processors/plugins/internet/ticket.pm
+++ processors/plugins/internet/ticket.pm
@@ -5,13 +5,17 @@ push (@commands_subs, sub {
   $answer = "http://trac.unknown-horizons.org/t/ticket/$answer" if ($target =~ /#unknown-horizons/);
   $answer = "https://github.com/grickit/Gambot/issues#issue/$answer" if ($target =~ /##Gambot/);
   $answer = "https://gna.org/bugs/index.php?$answer" if ($target =~ /#wesnoth/);
-  ACT("MESSAGE",$target,"$answer\n"); 
+  ACT("MESSAGE",$target,"$receiver: $answer\n"); 
 
-  my $request = LWP::UserAgent->new;
-    $request->timeout(120);
-    $request->env_proxy;
-    $request->agent('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0)');
-  my $response = $request->get("$answer");
-  my $content = $response->decoded_content;
-  if ($content =~ /<title>((\n|\s|\r|\t|.)+)<\/title>/) { $answer="$1"; $answer=~s/(\n|\s|\r|\t)+/ /g; ACT("MESSAGE","$target","$answer"); }
+    my $request = LWP::UserAgent->new;
+      $request->timeout(120);
+      $request->env_proxy;
+      $request->agent('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0)');
+    my $response = $request->get("$answer");
+    my $content = $response->decoded_content;
+    if ($content =~ /<title>((\n|\s|\r|\t|.)+)<\/title>/) { 
+      $answer = $1;
+      $answer=~s/(\n|\s|\r|\t)+/ /g; 
+      ACT("MESSAGE","$target","$receiver: $answer"); 
+    }
 });
