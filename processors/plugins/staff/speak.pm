7825b57184b85a2828243b2d9495ca771b3978bc
Derek <derek@fran-ubuntu.(none)>
2011-04-29 10:46:38 -0400

    speak.pm was never updated

--- processors/plugins/staff/speak.pm
+++ processors/plugins/staff/speak.pm
@@ -1,14 +1,11 @@
-push (@commands_regexes, "$sl !tell ([$valid_chan_characters]+) (.+)\$");
-push (@commands_subs, sub {
+if (($event =~ /message/) && ($message =~ /^$sl !tell ([$valid_chan_characters]+) (.+)$/)) {
   CheckAuth($1,$hostname) ? ACT("MESSAGE",$1,$2) : Error($1); 
-});
+}
 
-push (@commands_regexes, "$sl !do ([$valid_chan_characters]+) (.+)\$");
-push (@commands_subs, sub {
+if (($event =~ /message/) && ($message =~ /^$sl !do ([$valid_chan_characters]+) (.+)$/)) {
   CheckAuth($1,$hostname) ? ACT("ACTION",$1,$2) : Error($1);
-});
+}
 
-push (@commands_regexes, "$sl !notify ([$valid_chan_characters]+) (.+)\$");
-push (@commands_subs, sub {
-  CheckAuth($1,$hostname) ? ACT("NOTICE",$1,$2) : Error($1);
-});
+if (($event =~ /message/) && ($message =~ /^$sl !notify ([$valid_chan_characters]+) (.+)$/)) {
+  ACT('MESSAGE',$target,"$receiver: $about"); 
+}
