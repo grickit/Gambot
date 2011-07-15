60f85d449be9f8290c36ab3c2dd6c686a7d4d7e5
Derek Hoagland <thegrickit@gmail.com>
2011-07-15 15:54:01 -0400

    temporary fix:

--- parsers/plugin_parser/plugins/staff/literal.pm
+++ parsers/plugin_parser/plugins/staff/literal.pm
@@ -1,10 +1,11 @@
 if ($message =~ /^$sl !literal (.+)$/) {
+  my $command = $1;
   if($message =~ /run_command>/) {
     ACT('MESSAGE',$target,"$sender: Sorry. Not even administrators may use the run_command API call. It's just too dangerous.");
   }
   else {
-    if (CheckAdmin()) {
-      ACT('LITERAL',undef,$1);
+    if ($hostname =~ /^wesnoth\/developer\/grickit$/) {
+      ACT('LITERAL',undef,$command);
     }
     else {
       ACT('MESSAGE',$target,"$sender: Who the hell do you think you are?");
