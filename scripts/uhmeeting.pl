383e264ff861c4519dc766bd4bb850187fba5fbd
Derek Hoagland <thegrickit@gmail.com>
2011-07-17 07:51:59 -0400

    Reworded the daily, and day-before announcements.

--- scripts/uhmeeting.pl
+++ scripts/uhmeeting.pl
@@ -11,11 +11,11 @@ while(defined sleep(1)) {
   my @days = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
 
   if (($wday < 6) && ($wday > 0) && ($hour == 17) && ($min == 0) && ($sec == 0)) {
-    print "send_server_message>PRIVMSG #unknown-horizons :Don't forget about the meeting \x02Sunday\x02 at \x0217:00 UTC\x02! It is currently $hour:$min UTC on $days[$wday].\n";
+    print "send_server_message>PRIVMSG #unknown-horizons :Don't forget about the meeting \x02Sunday\x02 at \x0217:00 UTC\x02! For reference, it is currently $hour:$min UTC on $days[$wday].\n";
   }
 
   elsif (($wday == 6) && (($hour == 12) || ($hour == 14) || ($hour == 16) || ($hour == 18) || ($hour == 20) || ($hour == 22)) && ($min == 0) && ($sec == 0)) {
-    print "send_server_message>PRIVMSG #unknown-horizons :Don't forget about the meeting \x02tomorrrow\x02 at \x0217:00 UTC\x02! It is currently $hour:$min UTC.\n";
+    print "send_server_message>PRIVMSG #unknown-horizons :Don't forget about the meeting \x02tomorrrow\x02 at \x0217:00 UTC\x02! For reference, it is currently $hour:$min UTC.\n";
   }
 
   elsif (($wday == 0) && ($hour < 16) && ($min == 0) && ($sec == 0)) {
