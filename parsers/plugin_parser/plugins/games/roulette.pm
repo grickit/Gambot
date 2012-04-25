aa6a3395a22e644aebc3b914f957c590a5e484aa
Derek Hoagland <thegrickit@gmail.com>
2012-04-25 13:28:40 -0400

    Make roulette use the new GAPIL calls

--- parsers/plugin_parser/plugins/games/roulette.pm
+++ parsers/plugin_parser/plugins/games/roulette.pm
@@ -1,29 +1,23 @@
 if ($message =~ /^$sl !roulette$/) {
-  ACT('LITERAL',undef,'get_variable_value>roulette'.$target.'chamber');
+  ACT('LITERAL',undef,'value_get>roulette>'.$target.'chamber');
   my $chamber = <STDIN>;
   $chamber =~ s/[\r\n\t\s]+$//;
 
-  if ($chamber <= 1) {
+  if (!$chamber) {
     ACT('MESSAGE',$target,'Starting new game of Russian Roulette for this channel.');
-    $chamber = int(rand(6))+1;
-    ACT('LITERAL',undef,'set_variable_value>roulette'.$target.'chamber>'.$chamber);
-    ACT('LITERAL',undef,'set_variable_value>roulette'.$target.'index>1');
+    ACT('LITERAL',undef,'value_set>roulette>'.$target.'chamber>'.(int(rand(6))+1));
+    ACT('LITERAL',undef,'value_set>roulette>'.$target.'index>0');
   }
 
-  ACT('LITERAL',undef,'get_variable_value>roulette'.$target.'index');
+  ACT('LITERAL',undef,'return value_increment>roulette>'.$target.'index>1');
   my $index = <STDIN>;
   $index =~ s/[\r\n\t\s]+$//;
 
-  my $chamberstring = $index;
-  ACT('MESSAGE',$target,"Pulling the trigger on chamber $chamberstring.");
-  if ($chamber == $index) {
+  ACT('MESSAGE',$target,"Pulling the trigger on chamber $index.");
+  if ($chamber eq $index) {
     ACT('MESSAGE',$target,"BANG! $sender is dead.");
-    ACT('LITERAL',undef,'set_variable_value>roulette'.$target.'chamber>1');
-    ACT('LITERAL',undef,'set_variable_value>roulette'.$target.'index>1');
+    ACT('LITERAL',undef,'value_set>roulette>'.$target.'chamber>0');
+    ACT('LITERAL',undef,'value_set>roulette>'.$target.'index>0');
   }
-  else {
-    ACT('MESSAGE',$target,"Click. $sender lives this time.");
-    $index++;
-    ACT('LITERAL',undef,'set_variable_value>roulette'.$target.'index>'.$index);
-  }
+  else { ACT('MESSAGE',$target,"Click. $sender lives this time."); }
 }
