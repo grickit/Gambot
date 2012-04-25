74c7b1548ff941d6694903547b969bf41ac57b6f
Derek Hoagland <thegrickit@gmail.com>
2012-04-25 10:33:31 -0400

    Variablize all these regexes

--- modules/Gambot/GAPIL.pm
+++ modules/Gambot/GAPIL.pm
@@ -25,18 +25,20 @@ sub parse_command {
   $command =~ s/[\r\n]+$//;
   debug_output("Received API call: $command");
 
+  my $validkey = '[a-zA-Z0-9_#-]+';
+
 
   if ($command =~ /^send_server_message>(.+)$/) {
     send_server_message($1);
     if ($1 =~ /^NICK (.+)$/) { set_core_value('nick',$1); }
   }
-  elsif ($command =~ /^send_pipe_message>([a-zA-Z0-9_#-]+)>(.+)$/) {
+  elsif ($command =~ /^send_pipe_message>($validkey)>(.+)$/) {
     send_pipe_message($1,$2);
   }
 
 
 
-  elsif ($command =~ /^get_core_value>([a-zA-Z0-9_#-]+)$/) {
+  elsif ($command =~ /^get_core_value>($validkey)$/) {
     if (my $value = &get_core_value($1)) {
       &send_pipe_message($pipeid,"$value");
     }
@@ -44,7 +46,7 @@ sub parse_command {
       &send_pipe_message($pipeid,"");
     }
   }
-  elsif ($command =~ /^get_config_value>([a-zA-Z0-9_#-]+)$/) {
+  elsif ($command =~ /^get_config_value>($validkey)$/) {
     if (my $value = &get_config_value($1)) {
       &send_pipe_message($pipeid,"$value");
     }
@@ -52,7 +54,7 @@ sub parse_command {
       &send_pipe_message($pipeid,"");
     }
   }
-  elsif ($command =~ /^get_variable_value>([a-zA-Z0-9_#-]+)$/) {
+  elsif ($command =~ /^get_variable_value>($validkey)$/) {
     if (my $value = &get_variable_value($1)) {
       &send_pipe_message($pipeid,"$value");
     }
@@ -63,19 +65,19 @@ sub parse_command {
 
 
 
-  elsif ($command =~ /^set_core_value>([a-zA-Z0-9_#-]+)>(.+)$/) {
+  elsif ($command =~ /^set_core_value>($validkey)>(.+)$/) {
     &set_core_value($1,$2);
   }
-  elsif ($command =~ /^set_config_value>([a-zA-Z0-9_#-]+)>(.+)$/) {
+  elsif ($command =~ /^set_config_value>($validkey)>(.+)$/) {
     &set_config_value($1,$2);
   }
-  elsif ($command =~ /^set_variable_value>([a-zA-Z0-9_#-]+)>(.+)$/) {
+  elsif ($command =~ /^set_variable_value>($validkey)>(.+)$/) {
     &set_variable_value($1,$2);
   }
 
 
 
-  elsif ($command =~ /^check_pipe_exists>([a-zA-Z0-9_#-]+)$/) {
+  elsif ($command =~ /^check_pipe_exists>($validkey)$/) {
     if (&check_pipe_exists($1)) {
       &send_pipe_message($pipeid,"1");
     }
@@ -83,10 +85,10 @@ sub parse_command {
       &send_pipe_message($pipeid,"");
     }
   }
-  elsif ($command =~ /^kill_pipe>([a-zA-Z0-9_#-]+)$/) {
+  elsif ($command =~ /^kill_pipe>($validkey)$/) {
     &kill_pipe($1);
   }
-  elsif ($command =~ /^run_command>([a-zA-Z0-9_#-]+)>(.+)$/) {
+  elsif ($command =~ /^run_command>($validkey)>(.+)$/) {
     &run_command($1,$2);
   }
 
@@ -107,11 +109,11 @@ sub parse_command {
     event_output("API call from $pipeid asked for a configuration reload.");
     &read_configuration_file(&get_core_value('home_directory') . '/configurations/' . &get_core_value('configuration_file'));
   }
-  elsif ($command =~ /^log>([a-zA-Z0-9_#-]+)>(.+)$/) {
+  elsif ($command =~ /^log>($validkey)>(.+)$/) {
     &normal_output($1,$2);
   }
 
-  elsif ($command =~ /^get_persistent_value>([a-zA-Z0-9_#-]+)>([a-zA-Z0-9_#-]+)$/) {
+  elsif ($command =~ /^get_persistent_value>($validkey)>($validkey)$/) {
     if (my $value = &get_persistent_value($1,$2)) {
       &send_pipe_message($pipeid,"$value");
     }
@@ -119,22 +121,22 @@ sub parse_command {
       &send_pipe_message($pipeid,"");
     }
   }
-  elsif ($command =~ /^set_persistent_value>([a-zA-Z0-9_#-]+)>([a-zA-Z0-9_#-]+)>(.+)$/) {
+  elsif ($command =~ /^set_persistent_value>($validkey)>($validkey)>(.+)$/) {
     &set_persistent_value($1,$2,$3);
   }
-  elsif ($command =~ /^del_persistent_value>([a-zA-Z0-9_#-]+)>([a-zA-Z0-9_#-]+)$/) {
+  elsif ($command =~ /^del_persistent_value>($validkey)>($validkey)$/) {
     &del_persistent_value($1,$2);
   }
-  elsif ($command =~ /^read_persistence_file>([a-zA-Z0-9_#-]+)$/) {
+  elsif ($command =~ /^read_persistence_file>($validkey)$/) {
     &read_persistence_file($1);
   }
-  elsif ($command =~ /^save_persistence_file>([a-zA-Z0-9_#-]+)$/) {
+  elsif ($command =~ /^save_persistence_file>($validkey)$/) {
     &save_persistence_file($1);
   }
   elsif ($command =~ /^save_all_persistence_files>$/) {
     &save_all_persistence_files();
   }
-  elsif ($command =~ /^check_persistence_domain_exists>([a-zA-Z0-9_#-]+)$/) {
+  elsif ($command =~ /^check_persistence_domain_exists>($validkey)$/) {
     if (&check_persistence_domain_exists($1)) {
       &send_pipe_message($pipeid,"1");
     }
@@ -143,13 +145,13 @@ sub parse_command {
     }
   }
 
-  elsif ($command =~ /^event_lock>([a-zA-Z0-9_#-]+)$/) {
+  elsif ($command =~ /^event_lock>($validkey)$/) {
     event_lock($pipeid,$1);
   }
-  elsif ($command =~ /^event_unlock>([a-zA-Z0-9_#-]+)$/) {
+  elsif ($command =~ /^event_unlock>($validkey)$/) {
     event_unlock($1);
   }
-  elsif ($command =~ /^check_event_lock_exists>([a-zA-Z0-9_#-]+)$/) {
+  elsif ($command =~ /^check_event_lock_exists>($validkey)$/) {
     if (&check_event_lock_exists($1)) {
       &send_pipe_message($pipeid,"1");
     }
