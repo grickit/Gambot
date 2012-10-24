c5879d8aba43bc26939fe9e6eba59a8b412b33fa
Derek Hoagland <thegrickit@gmail.com>
2012-10-24 13:46:39 -0400

    Check permissions in the proper place when kicking.

--- parsers/plugin_parser/plugins/staff/kick.pm
+++ parsers/plugin_parser/plugins/staff/kick.pm
@@ -1,7 +1,7 @@
 if ($message =~ /^${sl}${cm}kick $validNick ?(.+)?$/i) {
   my $kickmessage = "[requested by $sender]";
   $kickmessage .= ": $2" if $2;
-  if(authCheck($1,$hostname)) {
+  if(authCheck($target,$hostname)) {
     actOut('MESSAGE','chanserv',"op $target $botName");
     actOut('LITERAL',undef,"event_schedule>op$target>send_server_message>KICK $target $1 :$kickmessage");
     actOut('LITERAL',undef,"event_schedule>op$target>send_server_message>PRIVMSG chanserv :deop $target $botName");
