780b511b3d6321f78c6bc366942216325a2dcf1c
Derek Hoagland <thegrickit@gmail.com>
2013-09-15 08:24:49 -0400

    Make the roulette plugin use the fancy new CommandChild library.

--- parsers/plugin_parser/plugins/games/roulette.pm
+++ parsers/plugin_parser/plugins/games/roulette.pm
@@ -1,22 +1,20 @@
 if ($message =~ /^${sl}${cm}roulette$/i) {
-  actOut('LITERAL',undef,'value_get>roulette>'.$target.'chamber');
-  my $chamber = readInput();
+  my $chamber = $core->value_get('roulette',$target.':chamber');
 
   if (!$chamber) {
     actOut('MESSAGE',$target,'Starting new game of Russian Roulette for this channel.');
     $chamber = int(rand(6))+1;
-    actOut('LITERAL',undef,'value_set>roulette>'.$target.'chamber>'.$chamber);
-    actOut('LITERAL',undef,'value_set>roulette>'.$target.'index>0');
+    $core->value_set('roulette',$target.':chamber',$chamber);
+    $core->value_set('roulette',$target.':index',0);
   }
 
-  actOut('LITERAL',undef,'return value_increment>roulette>'.$target.'index>1');
-  my $index = readInput();
+  my $index = $core->value_increment('roulette',$target.':index',1);
 
   actOut('MESSAGE',$target,"Pulling the trigger on chamber $index.");
   if ($chamber eq $index) {
     actOut('MESSAGE',$target,"BANG! $sender is dead.");
-    actOut('LITERAL',undef,'value_set>roulette>'.$target.'chamber>0');
-    actOut('LITERAL',undef,'value_set>roulette>'.$target.'index>0');
+    $core->value_set('roulette',$target.':chamber',0);
+    $core->value_set('roulette',$target.':index',0);
   }
   else { actOut('MESSAGE',$target,"Click. $sender lives this time."); }
 }
