5e864489c749ef1cd98f8ae28e4d5b08a56a7d14
Derek Hoagland <thegrickit@gmail.com>
2013-09-15 08:10:14 -0400

    No more 'do' conflicts with qmark.

--- parsers/plugin_parser/plugins/staff/speak.pm
+++ parsers/plugin_parser/plugins/staff/speak.pm
@@ -2,7 +2,7 @@ if ($message =~ /^${sl}${cm}tell $validChannel (.+)$/i) {
   authCheck($1,$hostname) ? actOut('MESSAGE',$1,$2) : authError($sender,$target,$1);
 }
 
-if ($message =~ /^${sl}${cm}do $validChannel (.+)$/i) {
+if ($message =~ /^${sl}${cm}act $validChannel (.+)$/i) {
   authCheck($1,$hostname) ? actOut('ACTION',$1,$2) : authError($sender,$target,$1);
 }
 
