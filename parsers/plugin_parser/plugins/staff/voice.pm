8f7606365cf4097c3a66365aa42477685a7cbd25
Derek Hoagland <thegrickit@gmail.com>
2013-09-15 08:10:47 -0400

    I forgot this form of voice command.

--- parsers/plugin_parser/plugins/staff/voice.pm
+++ parsers/plugin_parser/plugins/staff/voice.pm
@@ -6,11 +6,19 @@ if ($message =~ /^${sl}${cm}voice(?:me)?$/i) {
   authCheck($target,$hostname) ? actOut('MESSAGE','chanserv',"voice $target $sender") : authError($sender,$target,$target);
 }
 
+if ($message =~ /^${sl}${cm}voice $validNick$/i) {
+  authCheck($target,$hostname) ? actOut('MESSAGE','chanserv',"voice $target $1") : authError($sender,$target,$target);
+}
+
 if ($message =~ /^${sl}${cm}devoice $validChannel $validNick$/i) {
   authCheck($1,$hostname) ? actOut('MESSAGE','chanserv',"devoice $1 $2") : authError($sender,$target,$1);
 }
 
 if ($message =~ /^${sl}${cm}devoice(?:me)?$/i) {
   authCheck($target,$hostname) ? actOut('MESSAGE','chanserv',"devoice $target $sender") : authError($sender,$target,$target);
+}
+
+if ($message =~ /^${sl}${cm}devoice $validNick$/i) {
+  authCheck($target,$hostname) ? actOut('MESSAGE','chanserv',"devoice $target $1") : authError($sender,$target,$target);
 }
 
