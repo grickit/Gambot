3d64b749a223c7b39ac77f65ace1b1adfc1d788c
Derek Hoagland <thegrickit@gmail.com>
2013-09-15 08:19:10 -0400

    Handle multiple dice in a smarter way.

--- parsers/plugin_parser/plugins/games/dice.pm
+++ parsers/plugin_parser/plugins/games/dice.pm
@@ -4,17 +4,6 @@ if ($message =~ /^${sl}${cm}d([0-9]+)$/i) {
 }
 
 if ($message =~ /^${sl}${cm}([0-9]+)d([0-9]+)$/) {
-  if ($1 <= 9000) {
-    my ($i, $rand, $answer) = (0,0,0);
-    while($i < $1) {
-      $rand = int(rand($2))+1;
-      $answer += $rand;
-      $i += 1;
-      #actOut('MESSAGE',"$target","$receiver: roll $i yielded $rand");
-    }
-    actOut('MESSAGE',$target,"$receiver: The total is $answer");
-  }
-  else {
-    actOut('MESSAGE',$target,"Help! $sender is trying to attack me! Their power level is OVER NINE THOUSAND!");
-  }
+  my $answer = int(rand($1*$2-$1))+$1;
+  actOut('MESSAGE',$target,"$receiver: The total is $answer");
 }
