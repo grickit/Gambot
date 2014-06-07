6c07e24d2fc8d75cc1c6772721a0c6916b7f33ae
Derek Hoagland <thegrickit@gmail.com>
2014-06-07 12:45:05 -0400

    More cleaning.

--- parsers/plugin_parser/plugins/games/buttcoins.pm
+++ parsers/plugin_parser/plugins/games/buttcoins.pm
@@ -1,7 +1,11 @@
+/*===== LOADING =====*/
 if(!$core->dictionary_exists('buttcoin:bank')) { $core->dictionary_load('buttcoin:bank'); }
 if(!$core->dictionary_exists('buttcoin:stats')) { $core->dictionary_load('buttcoin:stats'); }
 my $word_chosen = $core->value_get('buttcoin:stats','word') || 'the';
 
+
+
+/*===== BASIC FUNCS =====*/
 sub buttcoinAccountCheck {
   return $core->value_get('buttcoin:stats','active:'.uc($_[0])) || 0;
 }
@@ -20,6 +24,19 @@ sub buttcoinBalanceAdd {
 
 sub buttcoinBalanceSub {
   return $core->value_decrement('buttcoin:bank','balance:'.uc($_[0]),$_[1]);
+}
+
+
+
+/*===== COMPLEX FUNCS =====*/
+sub buttcoinMine {
+  $core->value_increment('buttcoin:bank','balance:'.uc($_[0]),1);
+  $core->value_increment('buttcoin:stats','mined:'.uc($_[0]),1);
+
+  my @word_list = ('the','that','a','and','for');
+  $word_chosen = $word_list[int(rand(5))];
+  $core->value_set('buttcoin:stats','word',$word_chosen);
+  $core->value_set('buttcoin:stats','timestamp',time);
 }
 
 sub buttcoinTransfer {
@@ -44,6 +61,9 @@ sub buttcoinBalance {
   actOut('MESSAGE',$target,"$receiver has $balance buttcoins (account $active).");
 }
 
+
+
+/*===== COMMANDS *=====/
 if ($message =~ /^${sl}${cm}buttcoin balance ?($validNick)?$/i) {
   buttcoinAccountActivate($sender);
   buttcoinBalance($1);
@@ -55,41 +75,20 @@ if ($message =~ /^${sl}${cm}buttcoin transfer ([0-9]+) ($validNick)$/i) {
 }
 
 if ($message =~ /\b$word_chosen\b/i and $event eq 'on_public_message') {
-  my $timestamp = $core->value_get('buttcoin:stats','timestamp');
-  $timestamp = time unless $timestamp;
+  my $timestamp = $core->value_get('buttcoin:stats','timestamp') || time;
   my $difference = (time-$timestamp);
 
   my $count = $core->value_increment('buttcoin:stats','count:'.$word_chosen,1);
   my $time = $core->value_increment('buttcoin:stats','time:'.$word_chosen,$difference);
   my $average = ($time/$count);
 
-  my $sender_backend = uc($sender);
+  actOut('DEBUG','##Gambot',"DEBUG: Buttcoin mined from \"$word_chosen\".");
 
-  my $sender_censored = $sender;
-  $sender_censored =~ s/[aeiou]/*/ig;
-  my $target_censored = $target;
-  $target_censored =~ s/[#aeiou]/*/ig;
-
-  $core->value_increment('buttcoin:bank','balance:'.$sender_backend,1);
-
-  actOut('DEBUG','##Gambot',"DEBUG: $sender_censored just mined in $target_censored. Chosen word was \"$word_chosen\". It took $difference seconds (word average is $average).");
-
-  my @word_list;
-  push(@word_list,'the');
- #push(@word_list,'this');
-  push(@word_list,'that');
-  push(@word_list,'a');
- #push(@word_list,'an');
-  push(@word_list,'and');
- #push(@word_list,'but');
- #push(@word_list,'or');
-  push(@word_list,'for');
- #push(@word_list,'so');
-
-  $word_chosen = $word_list[int(rand(5))];
-  $core->value_set('buttcoin:stats','word',$word_chosen);
-  $core->value_set('buttcoin:stats','timestamp',time);
+  buttcoinMine($sender);
 }
 
+
+
+/*===== EXIT =====*/
 $core->dictionary_save('buttcoin:bank');
 $core->dictionary_save('buttcoin:stats');
