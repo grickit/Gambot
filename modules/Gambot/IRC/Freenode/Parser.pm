d57b2edfd82586acaccbb708f94a2463087e4492
Derek Hoagland <thegrickit@gmail.com>
2014-06-28 13:56:47 -0400

    Let's actually make the parser properly for once.

---
+++ modules/Gambot/IRC/Freenode/Parser.pm
@@ -0,0 +1,117 @@
+#!/usr/bin/perl
+# Copyright (C) 2010-2013 by Derek Hoagland <grickit@gmail.com>
+# This file is part of Gambot.
+#
+# Gambot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Gambot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Gambot.  If not, see <http://www.gnu.org/licenses/>.
+
+### This file provides an object capable of parsing GAPIL.
+
+package Gambot::IRC::Freenode::Parser;
+use strict;
+use warnings;
+
+use Gambot::IRC::Freenode::Specifications;
+
+our $VERSION = 1.0;
+our @ISA = qw(Exporter);
+our @EXPORT = qw();
+our @EXPORT_OK = qw();
+
+sub new {
+  my $class = shift;
+  my $self = {};
+
+  $self->{'core'} = shift;
+
+  bless($self,$class);
+  return $self;
+}
+
+our $VERSION = 1.0;
+our @ISA = qw(Exporter);
+our @EXPORT = qw();
+our @EXPORT_OK = qw();
+
+sub parse_message {
+  my ($self,$botname,$string) = @_;
+  my ($nick,$user,$host,$chan,$command,$message,$event,$redirect) = ('','','','','','','','');
+
+  if ($string =~ /^PING(.*)$/i) {
+    $event = 'on_server_ping';
+  }
+
+  elsif($string =~ /^:$validSenderHuman (PRIVMSG) $validChan :(.*)$/) {
+    ($nick,$user,$host,$chan,$command,$message,$event) = ($1,$2,$3,$5,$4,$6,'on_public_message');
+    if($chan eq $botname) { $event = 'on_private_message'; $chan = $nick; }
+
+    $redirect = $nick;
+    if($message =~ /@ ?([, $nickCharacters]+)$/) {
+      $redirect = $1;
+      $message =~ s/ ?@ ?$redirect$//;
+    }
+  }
+
+  elsif($string =~ /^:$validSenderHuman (NOTICE) $validChan :(.+)$/) {
+    ($nick,$user,$host,$chan,$command,$message,$event) = ($1,$2,$3,$5,$4,$6,'on_public_notice');
+    if($chan eq $botname) { $event = 'on_private_notice'; $chan = $nick; }
+  }
+
+  elsif($string =~ /^:$validSenderHuman (JOIN) :?$validChan$/) {
+    ($nick,$user,$host,$chan,$command,$event) = ($1,$2,$3,$5,$4,'on_join');
+  }
+
+  elsif ($string =~ /^:$validSenderHuman (PART) $validChan ?:?(.+)?$/) {
+    ($nick,$user,$host,$chan,$command,$message,$event) = ($1,$2,$3,$5,$4,$6,'on_part');
+    $message = '' unless $message;
+  }
+
+  elsif ($string =~ /^:$validSenderHuman (QUIT) ?:?(.+)?$/) {
+    ($nick,$user,$host,$command,$message,$event) = ($1,$2,$3,$4,$5,'on_quit');
+    $message = '' unless $message;
+  }
+
+  elsif ($string =~ /^:$validSenderHuman (MODE) $validChan (.+)$/) {
+    ($nick,$user,$host,$chan,$command,$message,$event) = ($1,$2,$3,$5,$4,$6,'on_mode');
+  }
+
+  elsif ($string =~ /^:$validSenderHuman (NICK) :?$validNick$/) {
+    ($nick,$user,$host,$command,$message,$event) = ($1,$2,$3,$4,$5,'on_nick');
+  }
+
+  elsif ($string =~ /^:$validSenderHuman (KICK) $validChan ?:?(.+)?$/) {
+    ($nick,$user,$host,$chan,$command,$message,$sevent) = ($1,$2,$3,$5,$4,$6,'on_kick');
+    $message = '' unless $message;
+  }
+
+  elsif ($string =~ /^:$validSenderHuman (TOPIC) $validChan ?:?(.+)?$/) {
+    ($nick,$user,$host,$chan,$command,$message,$event) = ($1,$2,$3,$4,$5,$6,'on_topic');
+    $message = '' unless $message;
+  }
+
+  elsif ($string =~ /^:$validSenderServer ([a-zA-Z0-9]+) (.+?) ?:?(.+)?$/) {
+    ($nick,$command,$target,$message,$event) = ($1,$2,$3,$4,'on_server_message');
+    $message = '' unless $message;
+  }
+
+  elsif ($string =~ /^ERROR :(.+)$/) {
+    ($message) = ($1);
+    $event = 'on_server_error';
+  }
+
+  else {
+    return '';
+  }
+
+  return ($nick,$user,$host,$chan,$command,$message,$event,$redirect);
+}
---
+++ modules/Gambot/IRC/Freenode/Specifications.pm
@@ -0,0 +1,59 @@
+#!/usr/bin/perl
+# Copyright (C) 2010-2013 by Derek Hoagland <grickit@gmail.com>
+# This file is part of Gambot.
+#
+# Gambot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Gambot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Gambot.  If not, see <http://www.gnu.org/licenses/>.
+
+### This file provides an object capable of parsing GAPIL.
+
+package Gambot::IRC::Freenode::Specifications;
+use strict;
+use warnings;
+use FindBin;
+
+our $VERSION = 1.0;
+our @ISA = qw(Exporter);
+our @EXPORT = qw();
+our @EXPORT_OK = qw();
+
+our $VERSION = 1.0;
+our @ISA = qw(Exporter);
+our @EXPORT = qw(
+  $charactersNick
+  $charactersUser
+  $charactersHost
+  $charactersChan
+  $charactersServer
+  $validNick
+  $validUser
+  $validHost
+  $validChan
+  $validSenderHuman
+  $validSenderServer
+);
+our @EXPORT_OK = qw();
+
+our $charactersNick = 'A-Za-z0-9[\]\\`_^{}|-';
+our $charactersUser = 'A-Za-z0-9[\]\\`_^{}|.-';
+our $charactersHost = ':./A-Za-z0-9[\]\\`_^{}|-';
+our $charactersChan = '#A-Za-z0-9[\]\\`_^{}|-';
+our $charactersServer = 'a-zA-Z0-9\.';
+our $validNick = '(['.$charactersNick.']+)';
+our $validUser = '(['.$charactersUser.']+)';
+our $validHost = '(['.$charactersHost.']+)';
+our $validChan = '(['.$charactersChan.']+)';
+our $validSenderHuman = $validNick.'!~?'.$validUser.'@'.$validHost;
+our $validSenderServer = '(['.$charactersServer.']+)';
+
+1;
