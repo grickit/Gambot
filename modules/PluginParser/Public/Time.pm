1e45ac3e099317043b783d5bb0bdcf699c622c2c
Derek Hoagland <thegrickit@gmail.com>
2014-07-16 19:27:25 -0400

    Fix some crap with the time commands. (best commit message)

--- modules/PluginParser/Public/Time.pm
+++ modules/PluginParser/Public/Time.pm
@@ -12,7 +12,7 @@ sub match {
   if($core->{'event'} ne 'on_public_message' and $core->{'event'} ne 'on_private_message') { return ''; }
 
   if($core->{'message'} =~ /^time$/) {
-    return time_offset($core,0,$core->{'chan'},$core->{'target'});
+    return time_offset($core,'+0',$core->{'chan'},$core->{'target'});
   }
 
   elsif($core->{'message'} =~ /^time utc$/) {
@@ -20,7 +20,7 @@ sub match {
   }
 
   elsif($core->{'message'} =~ /^time ([+-][0-9]+)$/) {
-    return time_offset($core,$1*3600,$core->{'chan'},$core->{'target'});
+    return time_offset($core,$1,$core->{'chan'},$core->{'target'});
   }
 
   elsif($core->{'message'} =~ /^time unix$/) {
@@ -36,7 +36,7 @@ sub match {
 
 sub time_offset {
   my ($core,$offset,$chan,$target) = @_;
-  my $time = POSIX::strftime('%H:%M:%S',(gmtime(time+$offset)));
+  my $time = POSIX::strftime('%H:%M:%S',(gmtime(time+$offset*3600)));
 
   $core->{'output'}->parse("MESSAGE>${chan}>${target}: ${time} (UTC${offset})");
 }
