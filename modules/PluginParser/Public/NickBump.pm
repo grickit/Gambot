4059677854fc0e60922ddbe62a512305c8ea5bc4
Derek Hoagland <thegrickit@gmail.com>
2014-07-19 09:26:13 -0400

    Plugin for handling unavailability of the bot's nick

---
+++ modules/PluginParser/Public/NickBump.pm
@@ -0,0 +1,24 @@
+package PluginParser::Public::NickBump;
+use strict;
+use warnings;
+use IRC::Freenode::Specifications;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(match);
+
+sub match {
+  my ($self,$core) = @_;
+
+  if($core->{'event'} ne 'on_server_message') { return ''; }
+
+  if($core->{'command'} eq '433') {
+    return bump($core,$core->{'botname'});
+  }
+
+  return '';
+}
+
+sub bump {
+  my ($core,$botname) = @_;
+
+  $core->{'output'}->parse("RENAME>${botname}_");
+}
