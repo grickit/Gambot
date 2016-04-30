8ef016854998fd33a6273ff865ab195b0a5bcad4
Derek Hoagland <thegrickit@gmail.com>
2016-04-30 00:09:31 -0400

    Handle HTTPS urls.

--- modules/PluginParser/Internet/FetchTitle.pm
+++ modules/PluginParser/Internet/FetchTitle.pm
@@ -10,7 +10,7 @@ sub match {
   if($core->{'event'} ne 'on_public_message' and $core->{'event'} ne 'on_private_message') { return ''; }
 
 
-  if($core->{'message'} =~ /^(http:\/\/.+)$/) {
+  if($core->{'message'} =~ /^(https?:\/\/.+)$/) {
     return title($core,$core->{'receiver_chan'},$core->{'target'},$1);
   }
 
