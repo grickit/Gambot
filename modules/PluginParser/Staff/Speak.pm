0abb7a65d873e1224bb1c98eecac172b4f230924
Derek Hoagland <thegrickit@gmail.com>
2016-04-30 09:49:23 -0400

    Add the other forms of speaking on command

--- modules/PluginParser/Staff/Speak.pm
+++ modules/PluginParser/Staff/Speak.pm
@@ -11,8 +11,16 @@ sub match {
   if($core->{'event'} ne 'on_public_message' and $core->{'event'} ne 'on_private_message') { return ''; }
 
 
-  if($core->{'message'} =~ /^say $validChanStrict (.+)$/) {
+  if($core->{'message'} =~ /^tell $validChanStrict (.+)$/) {
     return order_message($core,$1,$2);
+  }
+ 
+  elsif($core->{'message'} =~ /^act $validChanStrict (.+)$/) {
+    return order_act($core,$1,$2);
+  }
+
+  elsif($core->{'message'} =~ /^notify $validChanStrict (.+)$/) {
+    return order_notice($core,$1,$2);
   }
 
   return '';
@@ -25,3 +33,16 @@ sub order_message {
   $core->{'output'}->parse("MESSAGE>${chan}>${message}");
 }
 
+sub order_act {
+  my ($core,$chan,$message) = @_;
+  if(!$core->{'auth'}->test_sender($core,$chan)) { $core->{'auth'}->error($core,$chan); return ''; }
+
+  $core->{'output'}->parse("ACTION>${chan}>${message}");
+}
+
+sub order_notice {
+  my ($core,$chan,$message) = @_;
+  if(!$core->{'auth'}->test_sender($core,$chan)) { $core->{'auth'}->error($core,$chan); return ''; }
+
+  $core->{'output'}->parse("NOTICE>${chan}>${message}");
+}
